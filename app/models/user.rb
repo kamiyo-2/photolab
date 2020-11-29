577dcce5f88cea34d3b2d4629c81cba488087ef6
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-11-29 18:19:06 +0900

    新規登録ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :email,           format: { with: /@/}
+         validates :password,        length: { minimum: 6 }, confirmation: true,  format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+         validates :name, presence: true
+         validates :profile, presence: true
 end
