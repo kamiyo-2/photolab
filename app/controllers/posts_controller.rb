8ddc316ab873111374e3fa02bd4307847f51d0f7
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-11-29 18:27:48 +0900

    posts.views作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.valid?
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text, :post_image).merge(user_id: current_user.id)
+  end
+
 end
