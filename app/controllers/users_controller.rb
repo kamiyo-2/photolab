79c05bf47e82e41ae7f0379d5c040e0d3a30a5e1
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-06 17:05:47 +0900

    users.edit.html.erb作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,35 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!, only: [:show]
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @profile = user.profile
-    @posts = user.posts
+    @user = User.find(params[:id])
+   
+    @posts = @user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+        redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
+  end
+
+
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.valid?
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :user_image, :profile, :password)
   end
 
 end
