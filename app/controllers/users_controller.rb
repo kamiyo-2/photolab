30b00cfd486a81aa557dd768ffd5a5922fef21d6
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-11 13:49:41 +0900

    users.edit.html.erb編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :user_image, :profile, :pasrd)
+    params.require(:user).permit(:name, :user_image, :profile)
   end
 
 end
