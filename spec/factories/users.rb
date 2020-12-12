5b52416050b1c59a01be0e0c658fe43d76b8423b
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-12 20:52:09 +0900

    userテストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name              {"test"}
+    profile               {"profile"}
+    email                 {"test@example"}
+    password              {"000000"}
+    password_confirmation {password}
+  end
+end
